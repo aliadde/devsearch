f9920be4ddb2cf57b28dcc405e4f13a63985e394
ali <aamrt2024@gmail.com>
2024-11-14 04:39:49 +0000

    gggg

--- projects/views.py
+++ projects/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from .models import Project
+
 # Create your views here.
+
 def base(request):
       return render(request, 'projects/base.html' )
 
-def singlepage(request, id ):
-      projects = Project.objects.all()
-      for item in projects: 
-            if str(item.id) == id:
-                  project = item 
-      
-      return render(request, 'projects/singleproject.html',{'project': project } )
-      
 def projects(request):
       projects = Project.objects.all()
 
       context = {'projects':projects}
       return render(request, 'projects/projects.html', context )
       
+def singlepage(request, id ):
+      # method get : get that projects has that id 
+      project = Project.objects.get(id=id)
+      tags = project.tags.all()
+      return render(request, 'projects/singleproject.html',{'project': project ,'tags':tags} )
+
+
