82c12b6038684c582819197cd919d369303b3d12
ali <aamrt2024@gmail.com>
2024-11-20 11:24:07 +0000

    untile can not use static file because didn't set them to the url pattern in setting

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bmp#@vy14q#+gmudk4*86tun#piwgg7dl4+%m-dnpak_(uehs1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','/4524/' ]
 
 
 # Application definition
@@ -107,17 +107,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os 
-#  در هر حال باید url باشه ها . حذفش نکن هرگز
 
+# to set url for image or static files
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
+# if you have your static files are not in root  
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') 
-      
+
+# when you want to deploy you have to use this to make static file folder for django 
+# in server 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
