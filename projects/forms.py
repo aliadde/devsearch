e4962000424b68cffb92001f929b1e5efd679446
ali <aamrt2024@gmail.com>
2024-11-23 04:21:15 +0000

    projects app design upload and set seccessfully

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms  import widgets
 from .models import Project
 
 class ProjectForm(forms.ModelForm):
@@ -6,4 +7,25 @@ class ProjectForm(forms.ModelForm):
             model = Project
             fields = ['title','description','demo_link','source_link'
                       ,'tags','featured_image']
+            widgets = {
+                  'tags': forms.CheckboxSelectMultiple(),
+            }
+
             
+      # آخرم به زور فهمیدم چیشد توی این کده پایین 
+      def __init__(self,*args, **kwargs):
+            super(ProjectForm, self).__init__(*args,**kwargs)
+            for name , field in self.fields.items():
+                  field.widget.attrs.update(
+                        {
+                              'class':"input input--text" ,
+                               'id':"formInput#text" ,
+                        }
+                  )
+
+
+            # for single field adding class 
+            # self.fields['title'].widget.attrs.update({
+            #       'class':"input input--text" ,
+            #       id:"formInput#text" 
+            #       })
